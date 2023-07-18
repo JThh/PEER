201407af41b7f596a1b6ebe0cd340761cbf7597a
JThh <julius.han@outlook.com>
2023-07-18 09:53:34 -0400

    Add lora bash script

--- run_lora.sh
+++ run_lora.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-declare -a models=("ESM-2-8M" "ESM-2-35M" "ESM-2-150M" "ESM-2-15B") # "ESM-2-650M" "ESM-2-3B"
+declare -a models=("ESM-2-8M" "ESM-2-35M" "ESM-2-150M" "ESM-2-650M" "ESM-2-3B" "ESM-2-15B") # "ESM-2-650M" "ESM-2-3B"
 # output_dir="~/code/scratch/torchprotein_output/"
-task="fluorescence"
+task="subloc"
 log_dir="/home/yz979/code/scratch/torchprotein_logs/"
 yaml_file="config/single_task/ESM/${task}_ESM.yaml"
 
@@ -22,7 +22,7 @@ do
     touch "$log_file"
 
     # Modify the YAML file with the desired model and batch size
-    awk -v model="$model" -v batch_size="$batch_size" '/model:/ {sub(/ESM-.*/, model)} /batch_size:/ {sub(/[0-9]+/, batch_size)} 1' "$yaml_file" > temp_lora.yaml # && mv temp.yaml "$yaml_file"
+    awk -v model="$model" -v batch_size="$batch_size" '/model:/ {sub(/ESM-.*/, model)} /batch_size:/ {sub(/[0-9]+/, batch_size)} /path:/ {sub(/~\/scratch/, "~\/code\/scratch")} 1' "$yaml_file" > temp_lora.yaml # && mv temp.yaml "$yaml_file"
 
     # Execute the Python command
     CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master-port=29500 script/run_single.py -c temp_lora.yaml --seed 0 > "$log_file" 2>&1
